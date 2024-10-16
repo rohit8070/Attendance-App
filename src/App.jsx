6350945fe93bf10595cbec27a26118ccd9661243
Saddam848811 <sh848811@gmail.com>
2024-10-16 13:31:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/nav/nav";
 import Home from "./components/home/home";
+import Login from "./components/login/login";
 
 function App() {
   return (
     <>
       <Nav />
       <Home />
+      <Login />
     </>
   );
 }
